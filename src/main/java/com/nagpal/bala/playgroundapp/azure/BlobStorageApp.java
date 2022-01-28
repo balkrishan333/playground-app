0083eef6a8be4ce1d72655d1e1e393d4bad7b52d
--local <balkrishan.nagpal@gmail.com>
2022-01-28 14:08:41 +0530

    download file from azure using url only

--- src/main/java/com/nagpal/bala/playgroundapp/azure/BlobStorageApp.java
+++ src/main/java/com/nagpal/bala/playgroundapp/azure/BlobStorageApp.java
@@ -1,9 +1,6 @@
 package com.nagpal.bala.playgroundapp.azure;
 
-import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.*;
 import com.azure.storage.blob.models.BlobProperties;
 import com.azure.storage.blob.specialized.BlockBlobClient;
 import com.nagpal.bala.playgroundapp.json.validation.ValidateJSON;
@@ -25,9 +22,25 @@ public final class BlobStorageApp {
 
     public static void main(String[] args) {
 
+       // variousOperations();
+        downloadFileusingURL();
+
+    }
+
+    private static void downloadFileusingURL() {
+        BlobClientBuilder blobClientBuilder = new BlobClientBuilder();
+        blobClientBuilder.connectionString(CONNECTION_STRING_DEV_02);
+        //blobClientBuilder.blobName("last");
+        blobClientBuilder.endpoint("https://odxcentralusappsadev02.blob.core.windows.net/azure-webjobs-hosts/synctriggers/odx-centralus-dav-fa-dev02/last");
+        BlobClient blobClient = blobClientBuilder.buildClient();
+        blobClient.downloadToFile("/Users/bnagpal1/temp/url.txt");
+        System.out.println("=========== downloaded ===============");
+    }
+
+    private static void variousOperations() {
         //create Blob client
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(CONNECTION_STRING_DEV_02)
-                                                .buildClient();
+                .buildClient();
 
         // Get account name
         String accountName = blobServiceClient.getAccountName();
@@ -37,30 +50,29 @@ public final class BlobStorageApp {
         //BlobContainerClient containerClient = blobServiceClient.createBlobContainer(CONTAINER_NAME);
 
         //Get handle to exising container
-       // BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(CONTAINER_NAME);
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(CONTAINER_NAME);
 
         //upload file
-        /*{
+        {
             BlockBlobClient blockBlob = containerClient.getBlobClient(BLOB_NAME).getBlockBlobClient();
 
-            File file = new File("/Users/bnagpal1/temp/CCDs/Performance.xlsx");
+            File file = new File("/Users/bnagpal1/temp/cleanrostertest.csv");
             try (InputStream dataStream = new BufferedInputStream(new FileInputStream(file))) {
                 blockBlob.upload(dataStream, file.length(), true);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }*/
+        }
 
         //Blob handle to a blob object
-      //  BlobClient blobClient = containerClient.getBlobClient(BLOB_NAME);
+        //  BlobClient blobClient = containerClient.getBlobClient(BLOB_NAME);
         //download file
-      //  BlobProperties blobProperties1 = blobClient.downloadToFile("/Users/bnagpal1/temp/Performance.xlsx", true);
-     //   System.out.println("Metadata =========================");
-     //   Stream.of(blobProperties1.getMetadata()).forEach(System.out::println);
+        //  BlobProperties blobProperties1 = blobClient.downloadToFile("/Users/bnagpal1/temp/Performance.xlsx", true);
+        //   System.out.println("Metadata =========================");
+        //   Stream.of(blobProperties1.getMetadata()).forEach(System.out::println);
 
         //separate call is required to get content MD5
-      //  BlobProperties blobProperties = blobClient.getProperties();
-      //  System.out.println("MD 5 = " + Base64.getEncoder().encodeToString(blobProperties.getContentMd5()));
-
+        //  BlobProperties blobProperties = blobClient.getProperties();
+        //  System.out.println("MD 5 = " + Base64.getEncoder().encodeToString(blobProperties.getContentMd5()));
     }
 }
