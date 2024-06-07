ba36c0bf2c5b97a16b46d3d00f3870b83f08e3d4
Balkrishan Nagpal <balkrishan.nagpal@gmail.com>
2024-06-07 15:10:25 +0530

    Refactoring

--- src/main/java/com/nagpal/bala/playgroundapp/controllers/SpringConfigTestController.java
+++ src/main/java/com/nagpal/bala/playgroundapp/controllers/SpringConfigTestController.java
@@ -2,8 +2,6 @@ package com.nagpal.bala.playgroundapp.controllers;
 
 import com.nagpal.bala.playgroundapp.AppConfiguration;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +12,14 @@ import java.util.Map;
 
 @RestController
 @Log4j2
+@SuppressWarnings("unused")
 public class SpringConfigTestController {
 
     @Autowired
     private AppConfiguration appConfiguration;
 
     @RequestMapping(method = RequestMethod.GET, path = "/fetch-spring-config")
-    public ResponseEntity<String> hello() {
+    public ResponseEntity<String> fetchConfig() {
         log.info("Fetching spring configuration...");
         Map<String, String> secrets = appConfiguration.getClientSecrets();
         log.info("appConfiguration = {}", appConfiguration);
